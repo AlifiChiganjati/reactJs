089f4f8769a151b7a664786ab88028663e072271
alifichiganjati <alifichiganjati@gmail.com>
2023-10-29 01:50:14 +0700

    feat: add conditional rendering

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,4 +1,6 @@
-const AuthLayouts = ({ children, title }) => {
+import { Link } from "react-router-dom";
+
+const AuthLayouts = ({ children, title, type }) => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-full max-w-xs ">
@@ -7,9 +9,43 @@ const AuthLayouts = ({ children, title }) => {
           Welcome, add please your details
         </p>
         {children}
+        {/* {type === "login" ? "Don't have account?" : "I have an account?"} */}
+        {/* {type === "login" && ( */}
+        {/*   <Link to="/register" className="font-bold text-blue-600"> */}
+        {/*     Sign up */}
+        {/*   </Link> */}
+        {/* )} */}
+        {/**/}
+        {/* {type === "register" && ( */}
+        {/*   <Link to="/login" className="font-bold text-blue-600"> */}
+        {/*     Sign in */}
+        {/*   </Link> */}
+        {/* )} */}
+        <Navigation type={type} />
       </div>
     </div>
   );
 };
 
+const Navigation = ({ type }) => {
+  if (type === "login") {
+    return (
+      <p className="text-sm mt-5 text-center">
+        Dont have an account?{" "}
+        <Link to="/register" className="font-bold text-blue-600">
+          Sign up
+        </Link>
+      </p>
+    );
+  } else {
+    return (
+      <p className="text-sm mt-5 text-center">
+        I have an account?{" "}
+        <Link to="/login" className="font-bold text-blue-600">
+          Sign in
+        </Link>
+      </p>
+    );
+  }
+};
 export default AuthLayouts;
